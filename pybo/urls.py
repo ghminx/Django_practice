1f88ec03a7610f2a6ecc79d62cb76465434ccee1
ghminx <casu106@naver.com>
2025-08-06 17:54:58 +0900

    2-05 URL 별칭

--- pybo/urls.py
+++ pybo/urls.py
@@ -2,6 +2,9 @@ from django.urls import path
 
 from . import views 
 
+app_name = 'pybo'  # 앱 이름 설정
+
 urlpatterns = [
-     path('', views.index),
+    path('', views.index, name='index'),  # pybo/ 페이지에 index 함수 연결
+    path('<int:question_id>/', views.detail, name='detail'),
 ]
--- pybo/views.py
+++ pybo/views.py
@@ -1,10 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 # Create your views here.
 from django.http import HttpResponse   # 요청에 대한 응답 
-
+from .models import Question 
 
 # /pybo 페이지에 띄울 index 함수 
 # request : HTTP 요청 객체 
+# def index(request):
+#     return HttpResponse('pybo')
+
+
+
 def index(request):
-    return HttpResponse('pybo')
+    question_list = Question.objects.order_by('-create_date')  # 최신 질문 순으로 정렬
+    context = {'question_list': question_list}  # 템플릿에 전달할 데이터
+    return render(request, 'pybo/question_list.html', context)
+
+def detail(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)  # 질문 ID로 질문 객체 가져오기
+    context = {'question': question}
+    return render(request, 'pybo/question_detail.html', context)  # 질문 상세 페이지 렌더링
